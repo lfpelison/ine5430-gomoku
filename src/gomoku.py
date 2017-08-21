ba52748b0bfa5d3ec7fa0853793e0fece1883468
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-08-21 09:00:02 -0300

    Add src file

---
+++ src/gomoku.py
@@ -0,0 +1,9 @@
+'''
+Universidade Federal de Santa Catarina
+INE5430 - Inteligencia Artificial
+Autores: Andrei Donati, Igor Yamamoto, Luis Felipe Pelison
+'''
+
+
+if __name__ == '__main__':
+    print("Gomoku!")
