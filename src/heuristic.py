031cbcbb1647338ff61a75f657e8404709c43dd7
andrei donati <andrei donati>
2017-08-25 22:11:59 -0300

    User interface completed

--- src/heuristic.py
+++ src/heuristic.py
@@ -20,11 +20,13 @@ Utility = {'numberOfQuintet': [ 200000, ['xxxxx']],
 'numberOfTriplet_1Open': [ 15000, ['nxxxee', 'eexxxn']],
 'numberOfProbQuartet_2Opens': [ 7000, ['exexxe', 'exxexe']],
 'numberOfProbQuartet_1Open': [ 3000, ['nxexxe', 'nxxexe','exxexn','exexxn']],
-'numberOfDouble_2Opens': [ 1000, ['eexxe', 'exxee']],
+'numberOfDouble_2Opens': [ 500, ['eexxe', 'exxee']],
 'numberOfDouble_1Open': [ 400, ['nxxeee', 'eeexxn']],
 'numberOfProbTriplet_2Opens': [ 100, ['exexe']],
 'numberOfProbTriplet_1Open': [ 40, ['nxexee', 'eexexn']]
 }
+
+Finished = {'numberOfQuintet': [ 200000, ['xxxxx']]  }
 
 
 Heuristic = [
@@ -46,8 +48,8 @@ Heuristic = [
 ]
  
 
-def calculateHeuristic(state, heuristicValues, positionValuesHeuristic,player ):
-    '''  state= numpy matrix with 1's, 0's and -1's meaning the actual 
+def calculateHeuristic(state, heuristicValues, positionValuesHeuristic, player ):
+    '''  state= numpy matrix of 15x15  with 1's, 0's and -1's meaning the actual 
          state of the game
          heuristicValues =  dict with keys with the type of heuristc
          we are searching for, the values are a list. The first element of 
@@ -64,28 +66,45 @@ def calculateHeuristic(state, heuristicValues, positionValuesHeuristic,player ):
     sequenceHeuristic = 0
     positionHeuristic =0
     
+    newState = state.copy() #make a matrix 17 x 17
+    a= np.asarray([[2 for i in range(15)]]).T
+    newState = np.concatenate((a,np.concatenate((newState,a), axis=1)), axis=1).copy()
+    a= np.asarray([[2 for i in range(17)]])
+    newState = np.concatenate((a,np.concatenate((newState,a), axis=0)), axis=0).copy()
+    
+    notFinished = True
+    
     for values in heuristicValues.keys():
         
-        sequence = heuristicValues[values][1]
+        
         ValueSequence = heuristicValues[values][0]
         occurrences = 0
-       
-        occurrences += searchInList(makeDig(state, player), sequence)
-        occurrences += searchInList(makeCol(state, player), sequence)
-        occurrences += searchInList(makeLin(state, player), sequence)
-     
-                   
+        sequence = heuristicValues[values][1]
+        #print(values)
         
-        
+        for seq in sequence:
+            occurrences += searchInList(makeDig(newState, player), seq)
+            occurrences += searchInList(makeCol(newState, player), seq)
+            occurrences += searchInList(makeLin(newState, player), seq)
+            #print(seq)
+            #print(occurrences)
+           
         sequenceHeuristic += occurrences*ValueSequence
+        if(values=='numberOfQuintet' and occurrences>0):
+            notFinished= False
         
-    
+    newState = state.copy() 
+    if(player==1):    
+        np.place(newState, newState==-1, 0)
+        positionHeuristic = np.sum(np.multiply(newState,positionValuesHeuristic))
+    else:
+        np.place(newState, newState==1,0)
+        positionHeuristic = -np.sum(np.multiply(newState,positionValuesHeuristic))
+
         
-    positionHeuristic = np.sum(np.multiply(state,positionValuesHeuristic))
-    
-    
+     
     HeuristicValue= positionHeuristic + sequenceHeuristic
-    return HeuristicValue
+    return [HeuristicValue, notFinished]
 
     
 def makeDig(matrix, player):
@@ -99,21 +118,20 @@ def makeDig(matrix, player):
     for i in dig:
         str1=''
         for e in i:
-            
             if player==1:
                 if e==0:
-                    str1 += 'b'
-                elif (e==1 or e==2):
+                    str1 += 'e'
+                elif (e==-1 or e==2):
                     str1 += 'n'
-                elif(e==-1):
+                elif(e==1):
                     str1 +='x' 
             else:
                 if e==0:
-                    str1 = ''.join('b')
-                elif (e==-1 or e==2):
-                    str1 = ''.join('n')
-                elif(e==1):
-                    str1 = ''.join('x')       
+                    str1 += 'e'
+                elif (e==1 or e==2):
+                    str1 += 'n'
+                elif(e==-1):
+                    str1 +='x'     
         diagonal.append(str1)
         
     dig = [ matrix.diagonal(i) for i in range(matrix.shape[1]-5, -matrix.shape[1]+4,-1) ]
@@ -122,24 +140,25 @@ def makeDig(matrix, player):
         for e in i:
             if player==1:
                 if e==0:
-                    str1 += 'b'
-                elif (e==1 or e==2):
+                    str1 += 'e'
+                elif (e==-1 or e==2):
                     str1 += 'n'
-                elif(e==-1):
+                elif(e==1):
                     str1 +='x' 
             else:
                 if e==0:
-                    str1 = ''.join('b')
-                elif (e==-1 or e==2):
-                    str1 = ''.join('n')
-                elif(e==1):
-                    str1 = ''.join('x')       
+                    str1 += 'e'
+                elif (e==1 or e==2):
+                    str1 += 'n'
+                elif(e==-1):
+                    str1 +='x'        
         diagonal.append(str1)
+    #print(diagonal)
     
     return diagonal
     
 
-def makeCol(matrix, player):
+def makeLin(matrix, player):
     '''
          Make a vector with the collumns of the matrix 
     '''
@@ -150,23 +169,24 @@ def makeCol(matrix, player):
             
             if player==1:
                 if e==0:
-                    str1 += 'b'
-                elif (e==1 or e==2):
+                    str1 += 'e'
+                elif (e==-1 or e==2):
                     str1 += 'n'
-                elif(e==-1):
+                elif(e==1):
                     str1 +='x' 
             else:
                 if e==0:
-                    str1 = ''.join('b')
-                elif (e==-1 or e==2):
-                    str1 = ''.join('n')
-                elif(e==1):
-                    str1 = ''.join('x')       
+                    str1 += 'e'
+                elif (e==1 or e==2):
+                    str1 += 'n'
+                elif(e==-1):
+                    str1 +='x'       
         diagonal.append(str1)
+    #print(diagonal)
     return diagonal
     #matrix = matrix[1:len(matrix)-1,1:len(matrix)-1]
     
-def makeLin(matrix, player):
+def makeCol(matrix, player):
     '''
         Make a vector with the lines of the matrix 
     '''
@@ -178,19 +198,20 @@ def makeLin(matrix, player):
             
             if player==1:
                 if e==0:
-                    str1 += 'b'
-                elif (e==1 or e==2):
+                    str1 += 'e'
+                elif (e==-1 or e==2):
                     str1 += 'n'
-                elif(e==-1):
+                elif(e==1):
                     str1 +='x' 
             else:
                 if e==0:
-                    str1 = ''.join('b')
-                elif (e==-1 or e==2):
-                    str1 = ''.join('n')
-                elif(e==1):
-                    str1 = ''.join('x')       
+                    str1 += 'e'
+                elif (e==1 or e==2):
+                    str1 += 'n'
+                elif(e==-1):
+                    str1 +='x'       
         diagonal.append(str1)
+    #print(diagonal)
     return diagonal
     
     
--- src/userInterface.py
+++ src/userInterface.py
@@ -4,24 +4,60 @@ import time
 from tkinter.messagebox import *
 import random
 import numpy as np
+from functools import partial
+from heuristic import *
+
 
 class Game:
 
-    def __init__(self, master,player):
+    def __init__(self, master,utility, heuristic, finalState):
 
 #        self.flags = 60
+        
+        self.masterParameter = master
+        
         self.createButtons(master)
-
+        self.difficulty = 0
+        
+        self.utility = utility
+        self.heuristic = heuristic
+        
+        self.actualHeuristic = [0,0]
+        
+        self.finalState = finalState
+        
+        self.state= np.zeros((15,15))
+        
         self.bottomFrame = Frame(root)
         self.bottomFrame.grid(row=15, columnspan=155)
 
         self.quitBtn = Button(self.bottomFrame, text='Quit', command=self.quit)
-        self.quitBtn.grid(row=18, columnspan=2)
+        self.quitBtn.grid(row=18, columnspan=5)
         
-        self.player = []
-        self.player.append(player)
+        reset_with_arg = partial(self.reset, master)   
         
-        self.state= np.zeros((15,15))
+        self.Reset = Button(self.bottomFrame, text='Reset Game', command=reset_with_arg )
+        self.Reset.grid(row=19, columnspan=5)
+        
+        self.btn = Button(self.bottomFrame, text='My text' )
+        self.btn.config(state='disabled', relief=SUNKEN)
+        
+        self.heuristic_val = 0
+        
+        self.nextMovement = [0,0]
+               
+        self.player = self.choosePlayer()
+        self.pc = -1
+        
+        if(self.player==-1):
+            self.pc = 1
+            self.nextMove()
+            print('Heuristica PC: ' + str( self.actualHeuristic[0]) )
+            
+            self.buttons[self.findBt()][0].config(bg='blue')
+            self.buttons[self.findBt()][0].config(state='disabled', relief=SUNKEN)
+            self.state[self.buttons[self.findBt()][2]][self.buttons[self.findBt()][3]] =1
+       
     
 
     def createButtons(self, parent):
@@ -29,10 +65,11 @@ class Game:
         row = 0
         col = 0
         for x in range(0, 15*15):
-            status = 'danger'
+            
+            id=str(row) + '-' + str(col)
             self.buttons[x] = [
             Button(parent, bg='#8a8a8a',height = 2, width = 4),
-            [], #player
+            id, #player
             row,
             col]
             
@@ -48,43 +85,189 @@ class Game:
             for k in self.buttons:
                 self.buttons[k][0].grid(row= self.buttons[k][2], column= self.buttons[k][3])
 
-    #i don't know why but you'll get error if you remove these two functions
     def leftClick_w(self, x):
         return lambda Button: self.leftClick(x)
 
     def rightClick_w(self, x):
         pass
 #       return lambda Button: self.rightClick(x)
-
+        
     def leftClick(self, btn):
         
-        if ( self.player[-1]==1  and self.state[self.buttons[btn][2]][self.buttons[btn][3]]== 0) :
+        if ( self.player==1  and self.state[self.buttons[btn][2]][self.buttons[btn][3]]== 0) :
             self.buttons[btn][0].config(bg='blue')
             self.buttons[btn][0].config(state='disabled', relief=SUNKEN)
-            self.player.append(2)
             self.state[self.buttons[btn][2]][self.buttons[btn][3]] =1
+            self.calculate_heuristic_player()
+            print('Heuristica Player: ' + str( self.actualHeuristic[1]) )
+            if (self.is_gameover_pc()):
+                if (self.playAgainPlayer()==1):
+                    print('--------------------')
+                    self.Reset.invoke()
+                    return 1
+                else:
+                    self.quitBtn.invoke()
+                    return 1
+                
+            
+            
+            self.nextMove()
+            print('Heuristica PC: ' + str( self.actualHeuristic[0]) )                                    
+            
+            
+            if (self.is_gameover_player()):
+                if (self.playAgainPC()==1):
+                    print('--------------------')
+                    self.Reset.invoke()
+                    return 1
+                    
+                else:
+                    self.quitBtn.invoke()
+                    return 1
+            
+            
 
-        elif (self.player[-1] == 2 and self.state[self.buttons[btn][2]][self.buttons[btn][3]]== 0):
+        elif (self.player == -1 and self.state[self.buttons[btn][2]][self.buttons[btn][3]]== 0):
             self.buttons[btn][0].config(bg='red')
             self.buttons[btn][0].config(state='disabled', relief=SUNKEN)
-            self.player.append(1)
-            self.state[self.buttons[btn][2]][self.buttons[btn][3]] = 2
+            self.state[self.buttons[btn][2]][self.buttons[btn][3]] = -1
+            self.calculate_heuristic_player()
+            print('Heuristica Player: ' + str( self.actualHeuristic[1]) )
+            if (self.is_gameover_pc()):
+                if (self.playAgainPlayer()==1):
+                    print('--------------------')
+                    self.Reset.invoke()
+                    return 1
+                else:
+                    self.quitBtn.invoke()
+                    return 1
             
+            self.nextMove()
+            print('Heuristica PC: ' + str( self.actualHeuristic[0]) )           
+            
+            
+            if (self.is_gameover_player()):
+                if (self.playAgainPC()==1):
+                    print('--------------------')
+                    self.Reset.invoke()
+                    return 1
+                else:
+                    self.quitBtn.invoke()
+                    return 1
+        
+    def choosePlayer(self):
+        msg = 'Do you want to play first?'
+        answer = askquestion('PLay First',msg)
+        if answer == 'yes':
+            return 1
+        else:
+            return -1
+    
+    def playAgainPlayer(self):
+        msg = 'Congratulations! You win! Do you want to play again?'
+        answer = askquestion('play again',msg)
+        if answer == 'yes':
+            return 1
+        else:
+            return -1
+    
+    def playAgainPC(self):
+        msg = ':-( You lose!  Do you want to play again?'
+        answer = askquestion('play again',msg)
+        if answer == 'yes':
+            return 1
+        else:
+            return -1
+        
+    def is_gameover_pc(self):
+        # A board is terminal if it is won or there are no empty spaces.
+        if (0 in self.state and calculateHeuristic(self.state, self.finalState, self.heuristic, self.player)[1] ):
+            return False
+        else:
+            
+            return True
+    
+    def is_gameover_player(self):
+        # A board is terminal if it is won or there are no empty spaces.
+        if ( 0 in self.state  and calculateHeuristic(self.state, self.finalState, self.heuristic, self.pc)[1] ):
+            
+            return False
+        else:
+            
+            return True
         
     
+    def findBt(self):
+        for b in self.buttons.keys():
+            if (self.buttons[b][2]==self.nextMovement[0] and self.buttons[b][3]==self.nextMovement[1]):
+                return b
+        
+    def calculate_heuristic_pc(self):
+        self.actualHeuristic[0]= calculateHeuristic(self.state, self.utility, self.heuristic, self.pc)[0]
+               
+        pass
+    
+    def calculate_heuristic_player(self):
+        self.actualHeuristic[1] = calculateHeuristic(self.state, self.utility, self.heuristic, self.player)[0]
+              
+        pass
+    
+    def nextMove(self):
+       self.nextMovement[0] =  np.random.randint(15, size=1)[0] #row
+       self.nextMovement[1] = np.random.randint(15, size=1)[0] #col
+       if (self.player ==1):
+            self.buttons[self.findBt()][0].config(bg='red')
+            self.buttons[self.findBt()][0].config(state='disabled', relief=SUNKEN)
+            self.state[self.buttons[self.findBt()][2]][self.buttons[self.findBt()][3]] =-1
+       else:
+            self.buttons[self.findBt()][0].config(bg='blue')
+            self.buttons[self.findBt()][0].config(state='disabled', relief=SUNKEN)
+            self.state[self.buttons[self.findBt()][2]][self.buttons[self.findBt()][3]] =1
+            
+       self.calculate_heuristic_pc()
+       
+       return True
+    
+    def is_valid_move(self):
+       if self.state[self.nextMovement[0]][self.nextMovement[1]]== 0:
+          return True
+       else:
+         return False
+
+   
+          
+    def makeSons(self, x):
+        pass
            
 
     def quit(self):
         global root
-        root.quit()
+        root.destroy()
+        
+    def reset(self,master):
+        self.nextMovement = [0,0]
+        self.state= np.zeros((15,15))
+        for btn in self.buttons.keys():
+            self.buttons[btn][0].destroy()
+        self.createButtons(master)
+        self.player = self.choosePlayer()
+        if(self.player==-1):
+            
+            self.nextMove()
+            print('Heuristica PC: ' + str( self.actualHeuristic[0]) )
+            
+            self.buttons[self.findBt()][0].config(bg='blue')
+            self.buttons[self.findBt()][0].config(state='disabled', relief=SUNKEN)
+            self.state[self.buttons[self.findBt()][2]][self.buttons[self.findBt()][3]] =1
 
 
 def main():
     global root
     root = Tk()
-    root.geometry('580x650+0+0')
+    root.geometry('580x680+250+0')
     root.title('Gomoku Game')
-    game = Game(root,1)
+    game = Game(root,Utility, Heuristic, Finished)
+    root.iconbitmap("C:/Andrei/ine5430-gomoku/img/favicon.ico")
     root.mainloop()
 
 
