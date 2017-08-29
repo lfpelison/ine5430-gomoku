633890861238f4873d885b73a515f6647234c11a
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-08-29 12:33:12 -0300

    Optimze makeSons function

--- src/decideMove.py
+++ src/decideMove.py
@@ -5,8 +5,8 @@
 """
 from random import randint
 from heuristic import *
-
-use_api= True
+import numpy as np
+use_api = True
 
 def decideMove(State, numberOfPC, numberOfPlayer, levels=2):
         nextMovement = [0,0]
@@ -21,95 +21,66 @@ def reset():
 
     
 def applyMinimax(State, numberOfPC, numberOfPlayer, levels):
-
-  listOfHeuristics2= []
-  listOfHeuristics1= []
-  
-  listOfStates2= []
-  listOfStates1= []
-  listOfStates0= []
-  listOfStates0.append(State)
-  bestHeuristic1 = float('-inf')
-  bestState1 = State
-
-  listOfStates1.append( makeSons(listOfStates0[0], numberOfPC) )
-  idx = 0 
-  for state1 in listOfStates1[0]: #loop for the first level 
-      print("I'm on the index of state1: " + str(idx) )
-      listOfStates2.append( makeSons(state1, numberOfPlayer) )
-      bestState2 = listOfStates2[idx][0]
-      bestHeuristic2 = float('+inf')
-      idx2 = 0 
-      for state2 in listOfStates2[idx]:
-               
-          hr = -calculateHeuristicValue(state2, numberOfPlayer) + calculateHeuristic(state2, numberOfPC)
-          idx2+=1
-          if hr < bestHeuristic2:
-              bestHeuristic2 = hr
-              bestState2 = state2
-              
-          if bestHeuristic2 > bestHeuristic1:
-              bestHeuristic1 = bestHeuristic2
-              bestState1 = state2
-          if idx>0 and bestHeuristic2 < bestHeuristic1: #alpha prune
-              print('Pruned on iteration ' + str(idx2))
-              break
-              
-      listOfHeuristics1.append(bestHeuristic2)
-      print("The min heuristic for this state is: " + str(bestHeuristic2) )
-      print("--")
-      idx +=1
-      
-      
-  
-  
-      
-#  for hr in listOfHeuristics1:
-#          
-  
-  return findMovent(State, bestState1, numberOfPC)
     
+      listOfHeuristics2= []
+      listOfHeuristics1= []
+      
+      listOfStates2= []
+      listOfStates1= []
+      listOfStates0= []
+      listOfStates0.append(State)
+      bestHeuristic1 = float('-inf')
+      bestState1 = State
     
-
-  
-#def makeSons(state, numberToPlayWith):
-#    
-#    State= state.copy()
-#    allStates= []
-#      
-#    ite = []
-#    
-#    for i in range(0,8):
-#        ite.append( list(range(-i, i+1)) )
-#        
-##    ite = []
-##    old =[[0]]
-##    for i in range(0,7):
-##        actual = list(range(-i, i+1))
-##        ite.append( np.setdiff1d( actual, old) )
-##        old = actual
-#    
-##    for iteration in ite:
-##        for row in iteration:
-##            for col in iteration:
-##                if (State[row+7][col+7]==0):
-##                    newState= State.copy()
-##                    newState[row+7][col+7]= numberToPlayWith
-##                    allStates.append(newState )
-#       
-#    return allStates
+      listOfStates1.append( makeSons(listOfStates0[0], numberOfPC) )
+      idx = 0 
+      for state1 in listOfStates1[0]: #loop for the first level 
+          listOfStates2.append( makeSons(state1, numberOfPlayer) )
+          bestState2 = listOfStates2[idx][0]
+          bestHeuristic2 = float('+inf')
+          idx2 = 0 
+          for state2 in listOfStates2[idx]:
+                   
+              hr = -calculateHeuristicValue(state2, numberOfPlayer) + calculateHeuristic(state2, numberOfPC)
+              idx2+=1
+              if hr < bestHeuristic2:
+                  bestHeuristic2 = hr
+                  bestState2 = state2
+                  
+              if bestHeuristic2 > bestHeuristic1:
+                  bestHeuristic1 = bestHeuristic2
+                  bestState1 = state2
+              if idx>0 and bestHeuristic2 < bestHeuristic1: #alpha prune
+                  print('Pruned on iteration ' + str(idx2))
+                  break
+                  
+          listOfHeuristics1.append(bestHeuristic2)
+          print("idx: " + str(idx) +" - pr:" + str(idx2)+ " - bt hr: "  + str(bestHeuristic1) )
+          idx +=1         
+      
+      return findMovent(State, bestState1, numberOfPC)
+    
 
 def makeSons(state, numberToPlayWith):
     
-    State= state.copy()
+    currState = state.copy()
+    
+    proxState = currState.copy()
+    proxMatrix = np.ones((5,5))*3
+    for row in range(currState.shape[0]-4):
+        for col in range(currState.shape[1]-4):
+            if np.any(currState[row:row+5, col:col+5]):
+                proxState[row:row+5, col:col+5] += proxMatrix
+    
+    
     allStates= []
-     
-    for row in range(len(State)):
-        for col in range(len(State[row])):
-            if (State[row][col]==0):
-                newState= State.copy()
+    
+    for row in range(len(currState)):
+        for col in range(len(currState[row])):
+            if (proxState[row][col]%3==0 and proxState[row][col]>0):
+                newState= currState.copy()
                 newState[row][col]= numberToPlayWith
-                allStates.append(newState )
+                allStates.append(newState)
                 
                 
     return allStates
